0f16711243f057af500f778c433c2bfa04becd8a
Danny Bravman <dbravman@gmail.com>
2016-07-29 14:16:17 -0500

    puzzle24

--- puzzle24.rb
+++ puzzle24.rb
@@ -1,22 +1,24 @@
 class Puzzle24
   def initialize(min_digit, max_digit, n)
     @digits = (min_digit..max_digit).to_a
-    @permutations = []
-    get_next_digit("", @digits)
-    @permutations.sort[n-1]
+    p @digits.permutation.to_a.sort[n-1].join
+
+    # @permutations = []
+    # get_next_digit("", @digits)
+    # p @permutations.sort[n-1]
   end
 
-  def get_next_digit(number, digits)
-    digits.each do |digit|
-      new_number = number + digit.to_s
-      remaining_digits = digits - [digit]
-      if remaining_digits.length > 1
-        get_next_digit(new_number, remaining_digits)
-      else 
-        @permutations << new_number += remaining_digits[0].to_s
-      end
-    end
-  end
+  # def get_next_digit(number, digits)
+  #   digits.each do |digit|
+  #     new_number = number + digit.to_s
+  #     remaining_digits = digits - [digit]
+  #     if remaining_digits.length > 1
+  #       get_next_digit(new_number, remaining_digits)
+  #     else 
+  #       @permutations << new_number += remaining_digits[0].to_s
+  #     end
+  #   end
+  # end
 end
 
 Puzzle24.new(0, 2, 5)
