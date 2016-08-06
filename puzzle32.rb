25de4dbc6cd0fb9d12c6210da07a349a7fa6c4cc
Danny Bravman <dbravman@gmail.com>
2016-08-05 21:28:21 -0500

    puzzle32 hardcoding

--- puzzle32.rb
+++ puzzle32.rb
@@ -1,7 +1,32 @@
 class Puzzle32
-  def initialize(digits)
-    
+  def initialize
+    #1 x 4 = 4
+    #2 x 3 = 4
+    products = []
+    (1..9).each do |factor1|
+      (1234..9876).each do |factor2|
+        if check_pandigital(factor1, factor2, factor1 * factor2)
+          p "#{factor1} x #{factor2} = #{factor1 * factor2}" 
+          products << factor1 * factor2
+        end 
+      end
+    end
 
+    (12..98).each do |factor1|
+      (123..987).each do |factor2|
+        if check_pandigital(factor1, factor2, factor1 * factor2)
+          p "#{factor1} x #{factor2} = #{factor1 * factor2}"
+          products << factor1 * factor2
+        end 
+      end
+    end      
+
+    p products.uniq.reduce(:+)
+  end
+
+  def check_pandigital(factor1, factor2, product)
+    digits = factor1.to_s.split("") + factor2.to_s.split("") + product.to_s.split("")
+    return !digits.include?("0") && digits.length == 9 && digits.length == digits.uniq.length
   end
 end
 
@@ -22,4 +47,4 @@ end
   #   p products.reduce(:+)
   # end
 
-Puzzle32.new(9)
+Puzzle32.new
