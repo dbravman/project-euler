faaa1d02ab7319fbd08510a128a28a3d8736cf95
Danny Bravman <dbravman@gmail.com>
2016-08-05 22:24:41 -0500

    puzzle34 very slow

---
+++ puzzle34.rb
@@ -0,0 +1,28 @@
+class Puzzle34
+  def initialize
+    @factorials = {0 => 1}
+    (1..9).each do |n|
+      @factorials[n] = @factorials[n-1] * n
+    end
+    
+    curious_sum = 0
+    (3..999999).each do |n|
+      if curious(n)
+        curious_sum += n
+      end
+    end
+    p curious_sum
+  end
+
+  def curious(n)
+    sum = 0
+    p digits = n.to_s.split("")
+    digits.each do |digit|
+      sum += @factorials[digit.to_i]
+      return false if sum > n
+    end
+    sum == n
+  end
+end
+
+Puzzle34.new
